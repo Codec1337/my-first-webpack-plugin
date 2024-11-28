7296cf5392b0e718e8351bac0e4043cc1ed302c4
Codec <18237147876@163.com>
2024-11-28 22:19:50 +0800

    fix: 修复chalk无法require的问题

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const chalk = require('chalk')
+const chalk = require('chalk');
 
 class bugFreeWebpackPlugin {
     constructor() {}
@@ -10,7 +10,6 @@ class bugFreeWebpackPlugin {
         })
     }
 }
-
 
 module.exports = {
     bugFreeWebpackPlugin
